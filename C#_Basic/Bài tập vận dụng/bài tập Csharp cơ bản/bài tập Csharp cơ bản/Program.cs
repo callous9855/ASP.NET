6fbd179fb0d1473b28c7118a995f5f53061f99a9
huydinh <huydinh8246@gmail.com>
2018-11-30 16:50:40 +0700

    bài tập c# cơ bản

---
+++ C#_Basic/Bài tập vận dụng/bài tập Csharp cơ bản/bài tập Csharp cơ bản/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bài_tập_Csharp_cơ_bản
+{
+    class Helloworld
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 5;
+            b = 10;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("tong cua 15 và 20 la:");
+            Console.WriteLine(15 + 20);
+            Console.WriteLine("tong: {0}", a + b);
+            Console.WriteLine("hieu: {0}", a - b);
+            Console.WriteLine("nhan: {0}", a * b);
+            Console.WriteLine("chia: {0}", b / a);
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
