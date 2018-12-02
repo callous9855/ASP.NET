3a10c7b1fc02f49cd5220441fcab2cc27c943784
huydinh <huydinh8246@gmail.com>
2018-12-02 14:13:13 +0700

    bài tập very muộn :)))

---
+++ C#_Basic/Bài tập vận dụng/Cú pháp cơ bản/Cú pháp cơ bản/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cú_pháp_cơ_bản
+{
+    class Class1
+    {
+    }
+}
---
+++ C#_Basic/Bài tập vận dụng/bài tập Csharp cơ bản/bài tập Csharp cơ bản/Multipication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bài_tập_Csharp_cơ_bản
+{
+    class Multipication
+    {
+        int c;
+        public void multi()
+        {
+            Console.Write("nhap so bat ky ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("bang nhan cua " + c);
+            for (int d = 1; d <= 10; d = d + 1)
+            {
+                Console.WriteLine(c + " nhan " + d + " =" + c * d);
+            }
+            Console.ReadKey();
+        }
+    }
+}
